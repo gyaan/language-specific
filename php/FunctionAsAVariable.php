8357b49312fe6588dfdacf69918756365d52bf85
gyaan <gyaan1334@gmail.com>
2015-04-08 00:25:29 +0530

    function as varaible

---
+++ php/FunctionAsAVariable.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gyani
+ * Date: 8/4/15
+ * Time: 12:22 AM
+ * simple thing but earlier didn't have any idea about this found so thought to keep in my script repository
+ */
+
+
+function sayHello(){
+
+    echo "Hello!!!";
+
+}
+
+$functionName="sayHello";
+$functionName();
+
+
