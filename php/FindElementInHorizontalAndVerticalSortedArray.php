7c7aef13a971d262cbfbb389a573f438df2b878c
gyaan <gyaan1334@gmail.com>
2015-04-11 22:09:42 +0530

    find given element in a horizontal and vertical sorted matrix

--- php/FindElementInHorizontalAndVerticalSortedArray.php
+++ php/FindElementInHorizontalAndVerticalSortedArray.php
@@ -10,10 +10,10 @@ Class FindElementInHorizontalAndVerticalSortedArray{
 	private $column;
 	private $searchElement;
 	
-	public function __construct($matrixArray,$nuberOfRows,$numberOfColumn,$searchElementInMatrix){
+	public function __construct($matrixArray,$numberOfRows,$numberOfColumn,$searchElementInMatrix){
 
 		$this->matrix = $matrixArray;
-		$this->rows = $nuberOfRows;
+		$this->rows = $numberOfRows;
 		$this->column=$numberOfColumn;
 		$this->searchElement=$searchElementInMatrix;
 		
@@ -21,12 +21,12 @@ Class FindElementInHorizontalAndVerticalSortedArray{
 	
    public function findGivenElement(){
    	
-   	   $i=0; $j=$this->column;
+   	   $i=0; $j=$this->column-1;
    	   
-   	   while($i>$this->rows && $j>0){  //travers from right top corner to left bottom 
+   	   while($i<=$this->rows-1 && $j>=0){  //travers from right top corner to left bottom
    	   	
    	   	    if($this->matrix[$i][$j]==$this->searchElement)
-   	   	    	return ture;
+   	   	    	return true;
    	   	    else if($this->matrix[$i][$j]>$this->searchElement)
    	   	    	$j--;
    	   	    else 
@@ -45,14 +45,16 @@ Class FindElementInHorizontalAndVerticalSortedArray{
 
 
 $myMatrix=array();
-$column = 30;
+$column = 10;
 $rows = 30;
  for($i=0;$i<$rows;$i++)
  	for($j=0;$j<$column;$j++)
  		$myMatrix[$i][$j]=(int)$i+$j;
 
-$myClassObject = new FindElementInHorizontalAndVerticalSortedArray($myMatrix,$rows,$column,5);
-$find=$myClassObject.findGivenElement();
+//$searchKey=300;  not found
+$searchKey=21; //found
+$myClassObject = new FindElementInHorizontalAndVerticalSortedArray($myMatrix,$rows,$column,$searchKey);
+$find=$myClassObject->findGivenElement();
 
 if($find)
 	echo "found this element";
