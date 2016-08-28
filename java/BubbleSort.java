947c7fe8cb27cd7192f38c9262191a513d92a85f
gyaneshwar <gyaneshwarp@valyoo.in>
2016-08-28 22:20:54 +0530

    programes

--- java/BubbleSort.java
+++ java/BubbleSort.java
@@ -3,14 +3,30 @@
  */
 public class BubbleSort {
 
-
     public static void main(String[] args) {
-
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] arr = {32, 4, 3, 12, 89, 14, 7};
+        int[] a = bubbleSort.bubbleSort(arr);
+        bubbleSort.printArray(a);
     }
 
+    public int[] bubbleSort(int arr[]) {
+        int length = arr.length;
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+        return arr;
+    }
 
-    public void bubbleSort(int arr){
-
-
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "->");
+        }
     }
 }
