3c80dede7241e5de2d7f2013c60061de5048379e
gyaneshwar <gyaneshwarp@valyoo.in>
2016-08-30 13:07:01 +0530

    insert sort

---
+++ java/InsertionSort.java
@@ -0,0 +1,38 @@
+/**
+ * Created by lenskart on 28/08/16.
+ */
+public class InsertionSort {
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        int[] targetArray = {230, 1, 4, 2, 5, 34, 23, 67};
+        int[] sortedArray = insertionSort.insertionSort(targetArray);
+        insertionSort.printArray(sortedArray);
+    }
+
+    /**
+     * @param arr
+     * @return
+     */
+    public int[] insertionSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 1; i < length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && key < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        return arr;
+    }
+
+    /**
+     * @param arr
+     */
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "->");
+        }
+    }
+}
